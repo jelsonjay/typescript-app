acdb70088794d30081ae2853e433ec1bdaf070a7
Jelson J <jelson20@outlook.com>
2021-02-15 15:00:14 +0000

    commit changes

--- public/Generics.js
+++ public/Generics.js
@@ -7,12 +7,12 @@ const adduid = (obj) => {
 let mydoc = adduid({ name: 'mimoza', gender: 'femele', age: 39 });
 console.log(mydoc.name);
 const docTwo = {
-    id: 1,
+    uid: 1,
     resourceName: 'person',
     data: { name: 'jelson j', gender: 'male', age: 85 }
 };
 const docTree = {
-    id: 5,
+    uid: 5,
     resourceName: 'shoppingList',
     data: ['honey', 'sugar', 'coffee', 'tea', 'cake', 'bread']
 };
--- src/Generics.ts
+++ src/Generics.ts
@@ -9,21 +9,23 @@ let mydoc = adduid({name:'mimoza', gender: 'femele', age:39})
 
 console.log(mydoc.name)
 
+
+
 // Interface
 interface Resource<T>{
-id: number
+uid: number
 resourceName: string
 data:T
 }
 
-const docTwo: Resource<object>= {
-id: 1,
+const docTwo: Resource<object> = {
+uid: 1,
 resourceName: 'person',
 data: {name: 'jelson j', gender: 'male', age: 85}
 }
 
 const docTree: Resource<string[]> = {
-id: 5,
+uid: 5,
 resourceName: 'shoppingList',
 data: ['honey', 'sugar', 'coffee', 'tea', 'cake', 'bread']
 }
